a4fd2a065755bfb7e480ce0f365b7655d9a9d948
Zero <john.royer@gmail.com>
2024-06-30 16:18:50 +0800

    add simple HTTP server

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	ip := "127.0.0.1"
+	port := 8080
+
+	http.ListenAndServe(ip+strconv.Itoa(port), nil)
 }
